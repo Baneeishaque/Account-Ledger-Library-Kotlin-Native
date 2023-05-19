79f7a634878a5647a3316641d199c9c7873b8ef7
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-19 06:32:04 +0530

    compileMainCFile.ps1
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- compileMainCFile.ps1
+++ compileMainCFile.ps1
@@ -1,4 +1,6 @@
+.\gradlew.bat linkNative
 Import-Module "$env:ProgramFiles\Microsoft Visual Studio\2022\Preview\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell -VsInstallPath "$env:ProgramFiles\Microsoft Visual Studio\2022\Preview" -DevCmdArguments '-arch=x64'
+Set-Location -Path $(Split-Path $($MyInvocation.MyCommand.Path) -Parent)
 lib /DEF:lib\build\bin\native\debugShared\native.def /OUT:lib\build\bin\native\debugShared\native.lib
 cl.exe main.c lib\build\bin\native\debugShared\native.lib /Fe:lib\build\bin\native\debugShared\main.exe /Fo:lib\build\bin\native\debugShared\main.obj
