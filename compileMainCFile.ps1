6d8abb638f570252ab6a3344db112ccec37feb05
Banee Ishaque K <Baneeishaque@gmail.com>
2023-10-10 16:32:45 +0530

    Compile on Windows

--- compileMainCFile.ps1
+++ compileMainCFile.ps1
@@ -3,4 +3,4 @@ Import-Module "$env:ProgramFiles\Microsoft Visual Studio\2022\Preview\Common7\To
 Enter-VsDevShell -VsInstallPath "$env:ProgramFiles\Microsoft Visual Studio\2022\Preview" -DevCmdArguments '-arch=x64'
 Set-Location -Path $(Split-Path $($MyInvocation.MyCommand.Path) -Parent)
 lib /DEF:lib\build\bin\mingwX64\debugShared\account_ledger_lib.def /OUT:lib\build\bin\mingwX64\debugShared\account_ledger_lib.lib
-cl.exe main.c lib\build\bin\mingwX64\debugShared\account_ledger_lib.lib /Fe:lib\build\bin\mingwX64\debugShared\main.exe /Fo:lib\build\bin\mingwX64\debugShared\main.obj
+cl.exe main_windows.c lib\build\bin\mingwX64\debugShared\account_ledger_lib.lib /Fe:lib\build\bin\mingwX64\debugShared\main.exe /Fo:lib\build\bin\mingwX64\debugShared\main.obj
